29f6480b9bfc8c3a2f47bd3b88bde9cd73b97c2f
Yann MARCOU <creativeyann17@gmail.com>
2019-01-31 19:56:35 -0500

    add lazy loading

--- src/app/index.js
+++ src/app/index.js
@@ -1,22 +1,24 @@
-import React, { Component } from 'react';
+import React, { Component, lazy, Suspense } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 // i18n
 import { addLocaleData, IntlProvider } from 'react-intl';
 import localeEn from 'react-intl/locale-data/en';
 import localeFr from 'react-intl/locale-data/fr';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import i18nEn from '../i18n/en.json';
 import i18nFr from '../i18n/fr.json';
 // router
-import DefaultPage from '../pages/defaultPage';
 import { ROUTES, API_GUEST } from '../utils/constants';
 // actions
 import { apiServiceLoginRequest } from '../services/apiService/actions';
 // selectors
 import * as intlSelectors from '../services/intlService/selectors';
+import LoadingPage from '../pages/loadingPage';
 
 addLocaleData([...localeEn, ...localeFr]);
+
+const DefaultPage = lazy(() => import('../pages/defaultPage'));
 
 export const messages = {
   en: i18nEn,
@@ -34,9 +36,13 @@ class App extends Component {
     return (
       <React.Fragment>
         <IntlProvider locale={lang} messages={messages[lang]}>
-          <BrowserRouter>
-            <Route exact path={ROUTES.INDEX} component={DefaultPage} />
-          </BrowserRouter>
+          <Suspense fallback={<LoadingPage />}>
+            <BrowserRouter>
+              <Switch>
+                <Route exact path={ROUTES.INDEX} component={() => <DefaultPage />} />
+              </Switch>
+            </BrowserRouter>
+          </Suspense>
         </IntlProvider>
       </React.Fragment>
     );
